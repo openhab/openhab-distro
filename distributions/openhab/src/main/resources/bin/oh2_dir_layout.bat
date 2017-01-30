25d77e3e95b604013a257c0b56e2ad1143976763
heziegl <git@z5r.de>
2017-01-30 22:52:51 +0100

    Consider OPENHAB_* on startup. Closes #408
    if  OPENHAB_CONF, OPENHAB_RUNTIME, OPENHAB_USERDATA or OPENHAB_LOGDIR are set upon start their values are now used instead of defaults.

--- distributions/openhab/src/main/resources/bin/oh2_dir_layout.bat
+++ distributions/openhab/src/main/resources/bin/oh2_dir_layout.bat
@@ -2,10 +2,28 @@ rem DIRNAME is the directory of karaf, setenv, etc.
 CALL :removeSpacesFromPath "%DIRNAME%..\.."
 
 set OPENHAB_HOME=%RETVAL%
+
+:check_conf
+IF NOT [%OPENHAB_CONF%] == [] GOTO :conf_set
 set OPENHAB_CONF=%OPENHAB_HOME%\conf
+:conf_set
+
+:check_runtime
+IF NOT [%OPENHAB_RUNTIME%] == [] GOTO :runtime_set
 set OPENHAB_RUNTIME=%OPENHAB_HOME%\runtime
+:runtime_set
+
+:check_userdata
+IF NOT [%OPENHAB_USERDATA%] == [] GOTO :userdata_set
 set OPENHAB_USERDATA=%OPENHAB_HOME%\userdata
+:userdata_set
+
+:check_logs
+IF NOT [%OPENHAB_LOGDIR%] == [] GOTO :logs_set
 set OPENHAB_LOGDIR=%OPENHAB_USERDATA%\logs
+:logs_set
+
+
 set KARAF_HOME=%OPENHAB_RUNTIME%
 set KARAF_DATA=%OPENHAB_USERDATA%
 set KARAF_BASE=%OPENHAB_USERDATA%
