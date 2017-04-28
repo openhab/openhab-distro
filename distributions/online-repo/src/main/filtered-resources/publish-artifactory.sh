4258d2834e7dec8d1807544a407f1c9e88f2cb0e
Kai Kreuzer <kai@openhab.org>
2017-04-28 23:01:06 +0200

    added artifactory deploy script
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

---
+++ distributions/online-repo/src/main/filtered-resources/publish-artifactory.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Publish p2 repo to artifactory
+# Usage: publish-artifactory.sh user apikey  
+#
+ARTIFACTORY_URL=https://openhab.jfrog.io/openhab/online-repo-snapshot
+ARTIFACTORY_PACKAGE=online-repo
+PACKAGE_VERSION=${online.repo.version}
+PACKAGE_ARCHIVE=online-repo-${project.version}.zip
+PACKAGE_PATH=online-repo/$PACKAGE_VERSION
+
+if [ "x$1" != "x" ]; then
+    ARTIFACTORY_USER=$1
+fi
+if [ "x$2" != "x" ]; then
+    ARTIFACTORY_API_KEY=$2
+fi
+
+
+DIRNAME=`dirname "$0"`
+
+function main() {
+
+  cd $DIRNAME
+
+  echo "Uploading archive $PACKAGE_ARCHIVE"
+  response=$(curl -s -u ${ARTIFACTORY_USER}:${ARTIFACTORY_API_KEY} -H "X-Explode-Archive-Atomic: true" -X PUT "${ARTIFACTORY_URL}/repo.zip" -T ${PACKAGE_ARCHIVE} )
+  if [[ $response == *"success"* ]]
+  then
+    echo "Archive uploaded: $response"
+  else
+    echo "Error uploading: $response"
+    exit 1;
+  fi
+}
+
+main "$@"
