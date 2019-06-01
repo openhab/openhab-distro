dce6159626e8736d19139cb0c84f1dbd0a26cafe
Scott Rushworth <openhab@5iver.com>
2019-06-01 16:15:49 -0400

    Append existing EXTRA_JAVA_OPTS
    Signed-off-by: Scott Rushworth <openhab@5iver.com>

--- distributions/openhab/src/main/resources/bin/setenv.bat
+++ distributions/openhab/src/main/resources/bin/setenv.bat
@@ -121,7 +121,8 @@ set JAVA_OPTS=%JAVA_OPTS% ^
 :: set jvm options
 set EXTRA_JAVA_OPTS=-XX:+UseG1GC ^
   -Djava.awt.headless=true ^
-  -Dfile.encoding=UTF-8
+  -Dfile.encoding=UTF-8 ^
+  %EXTRA_JAVA_OPTS%
 
 :: set JAVA_HOME if not set yet
 rem Setup the Java Virtual Machine
