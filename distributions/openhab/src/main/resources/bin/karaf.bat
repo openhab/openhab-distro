fdd86d30a69f9c52e6f4d42fc88b69f84a8eb87e
Wouter Born <github@maindrain.net>
2021-02-07 10:06:22 +0100

    Suppress illegal reflective access operation warnings for java.time
    Suppresses the following warnings when the tokens of the OHC OAuth2 client are (de)serialized by the JSON Storage:
    
    WARNING: An illegal reflective access operation has occurred
    WARNING: Illegal reflective access by com.google.gson.internal.bind.ReflectiveTypeAdapterFactory (file:/openhab/userdata/cache/org.eclipse.osgi/27/0/bundleFile) to field java.time.LocalDateTime.date
    WARNING: Please consider reporting this to the maintainers of com.google.gson.internal.bind.ReflectiveTypeAdapterFactory
    WARNING: Use --illegal-access=warn to enable warnings of further illegal reflective access operations
    WARNING: All illegal access operations will be denied in a future release
    
    Signed-off-by: Wouter Born <github@maindrain.net>

--- distributions/openhab/src/main/resources/bin/karaf.bat
+++ distributions/openhab/src/main/resources/bin/karaf.bat
@@ -441,6 +441,7 @@ if "%KARAF_PROFILER%" == "" goto :RUN
                 --add-opens java.base/java.io=ALL-UNNAMED ^
                 --add-opens java.base/java.lang.reflect=ALL-UNNAMED ^
                 --add-opens java.base/java.text=ALL-UNNAMED ^
+                --add-opens java.base/java.time=ALL-UNNAMED ^
                 --add-opens java.desktop/java.awt.font=ALL-UNNAMED ^
                 --add-exports=java.base/sun.net.www.protocol.http=ALL-UNNAMED ^
                 --add-exports=java.base/sun.net.www.protocol.https=ALL-UNNAMED ^
